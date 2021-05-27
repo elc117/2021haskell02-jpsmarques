482ee363bfe0f57429c4eda1f8237a976497390a
João Pedro Marques <>
2021-05-27 03:52:09 +0000

    1 até a 4 ( 5 e 6 rascunhos )

--- haskell02.hs
+++ haskell02.hs
@@ -1,2 +1,34 @@
 -- Prática 02 de Haskell
--- Nome: 
+-- Nome: João Pedro 
+
+--Funções auxiliares:
+
+febre :: Float -> Bool 
+febre x = x > 37.8
+
+
+--1
+comFebre :: [Float] -> [Float]
+comFebre x = filter febre x
+
+--2
+-- filter (\x -> x>37.8)
+
+--3
+--map (\x -> "<li>"++ x ++"</li>")
+
+--4
+isCircleBigger :: Float -> Float -> Bool
+isCircleBigger x r = pi * r^2 > x
+
+bigCircles :: Float -> [Float] -> [Float]
+bigCircles x r = filter (isCircleBigger x) r
+
+
+--5
+--quarentena :: [(String,Float)] -> [(String,Float)]
+--quarentena nome temp = filter temp > 37.8
+
+--6
+idadesEm :: [Int] -> Int -> [Int]
+idadesEm idades ano = map (\idades -> ano - idades)
